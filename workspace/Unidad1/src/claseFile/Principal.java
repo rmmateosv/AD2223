89bf3724d29b35f7043dc0074836fbb810f7d2d5
rmmateosv@gmail.com <rmmateosv@gmail.com>
2022-09-16 11:54:25 +0200

    3

--- workspace/Unidad1/src/claseFile/Principal.java
+++ workspace/Unidad1/src/claseFile/Principal.java
@@ -1,6 +1,8 @@
 package claseFile;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
@@ -22,6 +24,9 @@ public class Principal {
 			System.out.println("2-Info Archivo/Carpeta");
 			System.out.println("3-Mostrar Carpeta");
 			System.out.println("4-Crear Carpeta");
+			System.out.println("5-Crear fichero");
+			System.out.println("6-Renombrar fichero");
+			System.out.println("7-MostrarTodo");
 			opcion = t.nextInt(); t.nextLine();
 			switch(opcion) {
 			case 1:
@@ -36,9 +41,113 @@ public class Principal {
 			case 4:
 				crearCarpeta();
 				break;
+			case 5:
+				crearFichero();
+				break;
+			case 6:
+				renombrarFichero();
+				break;
+			case 7:
+				mostrarTodo();
+				break;
 			}
 		}while(opcion!=0);
 		
+	}
+
+	private static void mostrarTodo() {
+		// TODO Auto-generated method stub
+		System.out.println("Introduce el nombre de una carpeta");
+		String carpeta = t.nextLine();
+		
+		File f = new File(carpeta);
+		if(f.exists() && f.isDirectory()) {
+			mostrarCarpeta(f,0);
+		}
+	}
+
+	private static void mostrarCarpeta(File f, int nivel) {
+		// TODO Auto-generated method stub
+		
+		//Mostramos el nombre de la carpeta actual
+		for(int i=0; i<nivel;i++) {
+			System.out.print("\t");
+		}
+		System.out.println("-"+f.getName());
+		//Obtenemos el contenido de la carpeta
+		File[] contenido = f.listFiles();
+		
+		for(int i=0;i<contenido.length;i++) {
+			if(contenido[i].isDirectory()) {
+				mostrarCarpeta(contenido[i],nivel+1);
+			}
+			else {
+				for(int j=0; j<nivel+1;j++) {
+					System.out.print("\t");
+				}
+				System.out.println("-"+contenido[i].getName());
+			}
+		}
+	}
+
+	private static void renombrarFichero() {
+		// TODO Auto-generated method stub
+		System.out.println("Introduce el nombre del fichero a renombrar");
+		String original = t.nextLine();
+		
+		File o = new File(original);
+		//Comprobams que existe
+		if(o.exists()) {
+			System.out.println("Introduce el nuevo nombre");
+			String nuevo = t.nextLine();
+			
+			File n = new File(nuevo);
+			//Comprobamos que no existe
+			if(!n.exists()) {
+				if(o.renameTo(n)) {
+					System.out.println("Fichero renombrado");
+				}
+				else {
+					System.out.println("Error: No se puede renombrar");
+				}
+			}
+			else {
+				System.out.println("Error, ya existe un fichero " + nuevo);
+			}
+		}
+		else {
+			System.out.println("Error, el fichero no existe");
+		}
+	}
+
+	private static void crearFichero() {
+		// TODO Auto-generated method stub
+		System.out.println("Introduce nombre de fichero");
+		String nombre = t.nextLine();
+		
+		//Creamos el objeto File
+		File f = new File(nombre);
+		//Comprobamos que no existe
+		if(!f.exists()) {
+			//Creamos el fichero
+			try {
+				
+				if(f.createNewFile()) {
+					System.out.println("Fichero creado correctamente");
+				}
+				else {
+					System.out.println("Error al crear el fichero");
+				}
+			} 
+			
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else {
+			System.out.println("Error: El fichero ya existe");
+		}
 	}
 
 	private static void crearCarpeta() {
