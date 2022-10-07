a05c0306c1026242abdd26d51effa3fbcbb31e40
rmmateosv@gmail.com <rmmateosv@gmail.com>
2022-10-07 11:17:16 +0200

    7_1

---
+++ workspace/Unidad1/src/FicherosAL/ADAleatorio.java
@@ -0,0 +1,156 @@
+package FicherosAL;
+
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.Date;
+
+import FicherosBinarios.Album;
+import FicherosTexto.ADTexto;
+import FicherosTexto.Artista;
+
+public class ADAleatorio {
+	String nombreF = "albumes.bin";
+	String nombreTmp = "albumes.tmp";
+	
+	ADTexto fArtistas = new ADTexto();
+	
+	public ADAleatorio() {
+		super();
+	}
+
+
+	public ArrayList<Album> obtenerAlbumes() {
+		// TODO Auto-generated method stub
+		ArrayList<Album> resultado = new ArrayList<>();
+		
+		RandomAccessFile f = null;
+		
+		try {
+			f = new RandomAccessFile(nombreF, "r");
+			//Bucle infinito => Parará cuando se produzca la
+			//excepción de fin de fichero EOF_EXCEPTION
+			while(true) {
+				Album a= new Album();
+				//Leemos dato a dato
+				//Id
+				a.setId(f.readInt());
+				
+				//Título - 50 caracteres
+				String titulo="";
+				for(int i=0;i<50;i++) {
+					titulo+=f.readChar();
+				}
+				//Limpiamos el título para quitar espacions en blanco
+				a.setTitulo(titulo.trim());
+				
+				//FEcha Publicación
+				a.setFechaP(new Date(f.readLong()));
+				
+				//Artista
+				String nombre="";
+				for(int i=0;i<50;i++) {
+					nombre+=f.readChar();
+				}
+				Artista artista = fArtistas.obtenerArtista(nombre.trim());
+				a.setArtista(artista);
+				
+				//Activo
+				a.setActivo(f.readBoolean());
+				
+				//Añadir el álbum al resultado
+				resultado.add(a);
+				
+			}
+			
+		} 
+		
+		catch (EOFException e) {
+			// TODO: handle exception
+		}
+		catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("No hay álbumes");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+			if(f!=null) {
+				try {
+					f.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return resultado;
+	}
+
+
+	public Album obtenerAlbum(String titulo, String nombreA) {
+		// TODO Auto-generated method stub
+		Album resultado=null;
+		
+		RandomAccessFile f =null;
+		try {
+			f=new RandomAccessFile(nombreF, "r");
+			//Saltamos el id, no es necesario leerlo
+			//Para ello posicionamos el apuntador
+			//sumando 4B a la posición actual
+			f.seek(f.getFilePointer()+4);
+			
+			//Leemos el título
+			String tit="";
+			for(int i=0;i<50;i++) {
+				tit+=f.readChar();
+			}
+			
+			//Saltamos la fecha, no es necesario leerla
+			//Para ello posicionamos el apuntador
+			//sumando 8B a la posición actual
+			f.seek(f.getFilePointer()+8);
+			
+			//Leemos el nombre del artista
+			String nombre="";
+			for(int i=0;i<50;i++) {
+				nombre+=f.readChar();
+			}
+			//Compruebo si coincide con el buscado
+			if(tit.trim().equalsIgnoreCase(titulo) && 
+			   nombre.trim().equalsIgnoreCase(nombreA)	) {
+				resultado = new Album();
+				resultado.setTitulo(titulo);
+				resultado.setArtista(fArtistas.obtenerArtista(nombreA));
+				//Leemos activo ¡¡ NO HAY COLOCAR EL APUNTADOR, ESTÁ AHÍ!!!
+				resultado.setActivo(f.readBoolean());
+				//Leemos el id
+				f.seek(f.getFilePointer()-213);
+				resultado.setId(f.readInt());
+				//Leemos la fecha
+				f.seek(f.getFilePointer()+100);
+				resultado.setFechaP(new Date(f.readLong()));
+				
+			}
+			else {
+				//Saltamos el campo activo y lo dejamos 
+				//posicionado al principio del siguiente registro
+				f.seek(f.getFilePointer()+1);
+			}
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return resultado;
+	}
+}
---
+++ workspace/Unidad1/src/FicherosAL/PrincipalAlbumes.java
@@ -0,0 +1,211 @@
+package FicherosAL;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import FicherosBinarios.Album;
+import FicherosTexto.ADTexto;
+import FicherosTexto.Artista;
+
+public class PrincipalAlbumes {
+	static Scanner t = new Scanner(System.in);
+	static ADAleatorio fAlbumes = new ADAleatorio();
+	static ADTexto fArtistas = new ADTexto();
+	static SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int opcion;
+		do {
+			System.out.println("Introduce opción:");
+			System.out.println("0-Salir");
+			System.out.println("1-Mostrar Álbumes");
+			System.out.println("2-Crear Álbum");
+			System.out.println("3-Modificar Artista");
+			System.out.println("4-Borrar Álbum");
+			System.out.println("5-Mostrar Álbum de artista");
+			System.out.println("6-Desactivar Álbum");
+			opcion = t.nextInt();
+			t.nextLine();
+			switch (opcion) {
+			case 1:
+				mostrarAlbumes();
+				break;
+			case 2:
+				crearAlbum();
+				break;
+			
+			case 3:
+				modificarArtista();
+				break;
+			case 4:
+				borrarAlbum();
+				break;
+			case 5:
+				mostrarAlbumArtista();
+				break;
+			case 6:
+				desactivarAlbum();
+				break;
+			}	
+
+		} while (opcion != 0);
+
+	}
+
+	private static void desactivarAlbum() {
+		// TODO Auto-generated method stub
+		mostrarAlbumes();
+		System.out.println("Id de álbum a desactivar");
+		int id = t.nextInt();t.nextLine();
+		Album al = fAlbumes.obtenerAlbum(id);
+		if(al!=null) {
+			if(al.isActivo()) {
+				if(fAlbumes.desactivarAlbum(al)) {
+					System.out.println("Álbum desactivado");
+				}
+				else {
+					System.out.println("Error al desactivar el álbum");
+				}
+			}
+			else {
+				System.out.println("El álbum no está activo");
+			}
+		}
+		else {
+			System.out.println("Error, no existe el álbum");
+		}
+	}
+
+	private static void mostrarAlbumArtista() {
+		// TODO Auto-generated method stub
+		// Mostrar Artistas
+		ArrayList<Artista> artistas = fArtistas.obtenerArtistas();
+		for (Artista a : artistas) {
+			a.mostrar();
+		}
+		System.out.println("Introduce el nombre del artista");
+		String nombre = t.nextLine();
+		Artista a = fArtistas.obtenerArtista(nombre);
+		if(a!=null) {
+			ArrayList<Album> albumes = fAlbumes.obtenerAlbumes(a);
+			for(Album al:albumes) {
+				al.mostrar();
+			}
+		}
+		else {
+			System.out.println("Error, no existe el artista");
+		}
+	}
+
+	private static void borrarAlbum() {
+		// TODO Auto-generated method stub
+		mostrarAlbumes();
+		System.out.println("Id de álbum a borrar");
+		int id = t.nextInt();t.nextLine();
+		Album al = fAlbumes.obtenerAlbum(id);
+		if(al!=null) {
+			 if(fAlbumes.borrarAlbum(al)) {
+				 System.out.println("Álbum borrado");
+			 }
+			 else {
+				 System.out.println("Error al borrar  el álbum");
+			 }
+		}
+		else {
+			System.out.println("Error, no existe el álbum");
+		}
+	}
+
+	private static void modificarArtista() {
+		// TODO Auto-generated method stub
+		mostrarAlbumes();
+		System.out.println("Id de álbum a moficar");
+		int id = t.nextInt();t.nextLine();
+		Album al = fAlbumes.obtenerAlbum(id);
+		if(al!=null) {
+			// Mostrar Artistas
+			ArrayList<Artista> artistas = fArtistas.obtenerArtistas();
+			for (Artista a : artistas) {
+				a.mostrar();
+			}
+			System.out.println("Introduce el nombre del nuevo artista");
+			String nombre = t.nextLine();
+			Artista ar = fArtistas.obtenerArtista(nombre);
+			if(ar!=null) {
+				if(fAlbumes.modificarArtista(al,ar)) {
+					System.out.println("Álbum modificado");
+				}
+				else {
+					System.out.println("Error, al modificar el álbum");
+				}
+			}
+			else {
+				System.out.println("Error, no existe el artista");
+			}
+			
+		}
+		else {
+			System.out.println("Error, no existe el álbum");
+		}
+	}
+
+	private static void crearAlbum() {
+		// TODO Auto-generated method stub
+		try {
+			// Mostrar Artistas
+			ArrayList<Artista> artistas = fArtistas.obtenerArtistas();
+			for (Artista a : artistas) {
+				a.mostrar();
+			}
+
+			// Pedir datos que idenfican al álbum
+			System.out.println("Introduce el artista:");
+			String nombreA = t.nextLine();
+			// Comprobar que existe el artista
+			Artista a = fArtistas.obtenerArtista(nombreA);
+			if (a != null) {
+				System.out.println("Título del álbum:");
+				String titulo = t.nextLine();
+				// Comprobar que no existe otro álbum con el mismo nombre y el
+				// mismo artista
+				Album al = fAlbumes.obtenerAlbum(titulo, nombreA);
+				if (al == null) {
+					// Creamos el objeto álbum
+					al = new Album();
+					al.setTitulo(titulo);
+					al.setArtista(a);
+					al.setActivo(true);
+					al.setId(fAlbumes.obtenerUltimoId() + 1);
+					System.out.println("Fecha último álbum (dd/MM/yyyy)");
+					al.setFechaP(formato.parse(t.nextLine()));
+					// Crear el álbum en el fichero
+					if (fAlbumes.crearAlbum(al)) {
+						System.out.println("Álbum " + al.getId() + " creado");
+					} else {
+						System.out.println("Error al crear el álbum");
+					}
+				} else {
+					System.out.println("Error, ya hay un álbum " + "con ese título para ese artista");
+				}
+
+			} else {
+				System.out.println("Error, no existe el artista");
+			}
+		} catch (ParseException e) {
+			// TODO: handle exception
+			System.out.println("Error, fecha incorrecta");
+		}
+	}
+
+	private static void mostrarAlbumes() {
+		// TODO Auto-generated method stub
+		ArrayList<Album> albumes = fAlbumes.obtenerAlbumes();
+		for (Album a : albumes) {
+			a.mostrar();
+		}
+	}
+
+}
