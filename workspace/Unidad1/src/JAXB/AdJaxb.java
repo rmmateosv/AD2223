38ccef3c1c1f42309c4ac13ed9f9f3219c43f5d3
rmmateosv@gmail.com <rmmateosv@gmail.com>
2022-10-19 14:31:04 +0200

    19_2

--- workspace/Unidad1/src/JAXB/AdJaxb.java
+++ workspace/Unidad1/src/JAXB/AdJaxb.java
@@ -14,78 +14,155 @@ import FicherosBinarios.Album;
 import FicherosTexto.Artista;
 
 public class AdJaxb {
-	
+
 	SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
-	
+
 	public AdJaxb() {
-		
+
 	}
 
 	public boolean crearXML(Artista a, ArrayList<Album> albumes) {
 		// TODO Auto-generated method stub
 		boolean resultado = false;
 		try {
-			//Creamos el artista de JAXB que va a pasar al fichero
+			// Creamos el artista de JAXB que va a pasar al fichero
 			ArtistaXML aXML = new ArtistaXML();
 			aXML.setNombre(a.getNombre());
 			aXML.setNumAlbumes(albumes.size());
-			
-	
-			for(Album al:albumes) {
-				//Crear objeto albumXML
+
+			for (Album al : albumes) {
+				// Crear objeto albumXML
 				AlbumXML alXML = new AlbumXML(al.getId(), al.getTitulo());
 				aXML.getAlbumes().add(alXML);
 			}
-			
-			//Ordenamos los albumes por fecha de publcación
+
+			// Ordenamos los albumes por fecha de publcación
 			albumes.sort(new Comparator<Album>() {
-	
+
 				@Override
 				public int compare(Album o1, Album o2) {
 					// TODO Auto-generated method stub
 					return o1.getFechaP().compareTo(o2.getFechaP());
 				}
-	
-				
+
 			});
 			aXML.setfPA(albumes.get(0).getFechaP());
-			aXML.setfUA(albumes.get(albumes.size()-1).getFechaP());
-			
-			//HAcemos el marshal		
-			Marshaller m = JAXBContext.newInstance(ArtistaXML.class)
-					                  .createMarshaller();
-			m.marshal(aXML,new File(a.getNombre().replace(" ", "")+
-					                "_jaxb.xml"));
-			resultado=true;
-			
+			aXML.setfUA(albumes.get(albumes.size() - 1).getFechaP());
+
+			// HAcemos el marshal
+			Marshaller m = JAXBContext.newInstance(ArtistaXML.class).createMarshaller();
+			m.marshal(aXML, new File(a.getNombre().replace(" ", "") + "_jaxb.xml"));
+			resultado = true;
+
 		} catch (JAXBException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return resultado;
 	}
 
 	public void mostrar(Artista a) {
 		// TODO Auto-generated method stub
-		File f = new File(a.getNombre().replace(" ", "")+
-					                "_jaxb.xml");
-		if(f.exists()) {
+		File f = new File(a.getNombre().replace(" ", "") + "_jaxb.xml");
+		if (f.exists()) {
 			try {
-				Unmarshaller um = JAXBContext.newInstance(ArtistaXML.class)
-						          .createUnmarshaller();
+				Unmarshaller um = JAXBContext.newInstance(ArtistaXML.class).createUnmarshaller();
 				ArtistaXML art = (ArtistaXML) um.unmarshal(f);
 				art.mostrar();
 			} catch (JAXBException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-		}
-		else {
+
+		} else {
 			System.out.println("No se ha generado información del artista");
 		}
 	}
 
-	
+	public boolean addAlbum(String nombre, String titulo) {
+		// TODO Auto-generated method stub
+		boolean resultado = false;
+
+		File f = new File(nombre.replace(" ", "") + "_jaxb.xml");
+		if (f.exists()) {
+			try {
+				Unmarshaller um = JAXBContext.newInstance(ArtistaXML.class).createUnmarshaller();
+				ArtistaXML art = (ArtistaXML) um.unmarshal(f);
+				
+				//Incrementamos el nº de álbumes
+				art.setNumAlbumes(art.getNumAlbumes()+1);
+				
+				//Creamos el nuevo álbum
+				AlbumXML al = new AlbumXML(art.getAlbumes()
+						.get(art.getAlbumes().size()-1).getId()+1,
+						titulo);
+				art.getAlbumes().add(al);
+				
+				//HAcemos el marshall
+				Marshaller m = JAXBContext.newInstance(ArtistaXML.class).createMarshaller();
+				m.marshal(art, new File(nombre.replace(" ", "") + "_jaxb.xml"));
+				resultado = true;
+				
+			} catch (JAXBException e) {
+					// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} 
+
+		return resultado;
+	}
+
+	public ArrayList<AlbumXML> obtenerAlbumes(String nombre) {
+		// TODO Auto-generated method stub
+		ArrayList<AlbumXML> resultado = new ArrayList();
+		
+		File f = new File(nombre.replace(" ", "") + "_jaxb.xml");
+		if (f.exists()) {
+			
+				try {
+					Unmarshaller um = JAXBContext.newInstance(ArtistaXML.class)
+							.createUnmarshaller();
+					ArtistaXML art = (ArtistaXML) um.unmarshal(f);
+					return art.getAlbumes();
+							
+				} catch (JAXBException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			
+		}
+		return resultado;
+	}
+
+	public boolean borrarAlbum(String nombre, int id) {
+		// TODO Auto-generated method stub
+		boolean resultado = false;
+		File f = new File(nombre.replace(" ", "") + "_jaxb.xml");
+		if (f.exists()) {
+			
+				try {
+					Unmarshaller um = JAXBContext.newInstance(ArtistaXML.class)
+							.createUnmarshaller();
+					ArtistaXML art = (ArtistaXML) um.unmarshal(f);
+					
+					//Borrar el álbum
+					for(AlbumXML al:art.getAlbumes()) {
+						if(id==al.getId()) {
+							indice
+						}
+					}
+							
+				} catch (JAXBException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			
+		}
+		return resultado;
+	}
+
 }
--- workspace/Unidad1/src/JAXB/Principal.java
+++ workspace/Unidad1/src/JAXB/Principal.java
@@ -26,6 +26,8 @@ public class Principal {
 			System.out.println("0-Salir");
 			System.out.println("1-Generar info de artista");
 			System.out.println("2-Mostrar info de artista");
+			System.out.println("3-Añadir album");
+			System.out.println("4-Borrar album");
 			opcion = t.nextInt();
 			t.nextLine();
 			switch (opcion) {
@@ -35,10 +37,71 @@ public class Principal {
 			case 2:
 				MostrarInfoArtista();
 				break;
+			case 3:
+				AddAlbum();
+				break;
+			case 4:
+				borrarAlbum();
+				break;
 			}
 			
 		} while (opcion != 0);
 
+	}
+
+	private static void borrarAlbum() {
+		// TODO Auto-generated method stub
+		ArrayList<Artista> artistas = fArtistas.obtenerArtistas();
+		for(Artista a: artistas) {
+			a.mostrar();
+		}
+		System.out.println("Introduce Nombre:");
+		Artista a = fArtistas.obtenerArtista(t.nextLine());
+		if(a!=null) {
+			ArrayList<AlbumXML> albumes = 
+					 fInfoArtista.obtenerAlbumes(a.getNombre());
+			for(AlbumXML al:albumes) {
+				al.mostrar();
+			}
+			System.out.println("Introduce el id a borrar");
+			int id = t.nextInt(); t.nextLine();
+			if(fInfoArtista.borrarAlbum(a.getNombre(),id)) {
+				System.out.println("Álbum borrado");
+			}
+			else {
+				System.out.println("Error, puede que no exista "
+						+ "información del artista");
+			}
+			
+		}
+		else {
+			System.out.println("Error, no existe el artista");
+		}
+	}
+
+	private static void AddAlbum() {
+		// TODO Auto-generated method stub
+		ArrayList<Artista> artistas = fArtistas.obtenerArtistas();
+		for(Artista a: artistas) {
+			a.mostrar();
+		}
+		System.out.println("Introduce Nombre:");
+		Artista a = fArtistas.obtenerArtista(t.nextLine());
+		if(a!=null) {
+			System.out.println("Título del álbum");
+			String titulo = t.nextLine();
+			if(fInfoArtista.addAlbum(a.getNombre(),titulo)) {
+				fInfoArtista.mostrar(a);
+			}
+			else {
+				System.out.println("Error, puede que no exista "
+						+ "información del artista");
+			}
+			
+		}
+		else {
+			System.out.println("Error, no existe el artista");
+		}
 	}
 
 	private static void MostrarInfoArtista() {
