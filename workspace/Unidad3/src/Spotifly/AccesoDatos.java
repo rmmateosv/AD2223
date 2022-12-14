cac8453c13f48ee79829c04418608fbefb98d6bf
rmmateosv@gmail.com <rmmateosv@gmail.com>
2022-12-14 13:24:09 +0100

    14_2

--- workspace/Unidad3/src/Spotifly/AccesoDatos.java
+++ workspace/Unidad3/src/Spotifly/AccesoDatos.java
@@ -1,15 +1,19 @@
 package Spotifly;
 
+import com.mongodb.ConnectionString;
 import com.mongodb.MongoException;
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
 public class AccesoDatos {
 	private MongoClient cluster = null;
 	private MongoDatabase bd = null;
-	
+	private String cadenaCNX = "mongodb+srv://<username>:<password>@accesodatos.c38r0xl.mongodb.net/?retryWrites=true&w=majority";
 	public AccesoDatos() {
-		
+		//Conectar con el cluster AccesoDatos de MongoAtlas
+		cluster = MongoClients.create(
+				new ConnectionString(cadenaCNX));
 	}
 
 	public MongoDatabase getBd() {
