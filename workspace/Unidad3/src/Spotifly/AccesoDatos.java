d3092ec985d2b167e4065c2e70d7023d815b9173
rmmateosv@gmail.com <rmmateosv@gmail.com>
2022-12-14 13:16:51 +0100

    14_1

--- workspace/Unidad3/src/Spotifly/AccesoDatos.java
+++ workspace/Unidad3/src/Spotifly/AccesoDatos.java
@@ -1,5 +1,34 @@
 package Spotifly;
 
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
 public class AccesoDatos {
+	private MongoClient cluster = null;
+	private MongoDatabase bd = null;
+	
+	public AccesoDatos() {
+		
+	}
+
+	public MongoDatabase getBd() {
+		return bd;
+	}
+
+	public void setBd(MongoDatabase bd) {
+		this.bd = bd;
+	}
+	
+	public void cerrar() {
+		try {
+			cluster.close();
+			
+		} catch (MongoException e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+	}
+	
 
 }
