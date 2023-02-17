f3b38bbfbe6dc4c1210c7dd882903490d818a040
rmmateosv@gmail.com <rmmateosv@gmail.com>
2023-02-17 12:40:50 +0100

    17

--- workspace/Unidad5/src/instituto/Principal.java
+++ workspace/Unidad5/src/instituto/Principal.java
@@ -1,12 +1,14 @@
 package instituto;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Principal {
 	static AccesoDatos ad = new AccesoDatos();
-	static SimpleDateFormat s = new SimpleDateFormat("dd/MM/yyyy");
+	static SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
 	static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
@@ -28,11 +30,57 @@ public class Principal {
 				case 1:
 					mostrarAlumnos();
 					break;
+				case 2:
+					ponerNota();
+					break;
 				
 
 				}
 			} while (opcion != 0);
 		}
+	}
+
+	private static void ponerNota() {
+		// TODO Auto-generated method stub
+		try {
+			mostrarAlumnos();
+			System.out.println("Introduce id:");
+			int id = sc.nextInt(); sc.nextLine();
+			Alumno a = ad.obtenerAlumno();
+			if(a!=null) {
+				mostrarAsignaturas();
+				System.out.println("Introduce asignatura");
+				String asig = sc.nextLine();
+				Asignatura as = ad.obtenerAsignatura(asig);
+				if(as!=null) {
+					System.out.println("Fecha (dd/mm/yyyy):");
+					Date f = formato.parse(sc.nextLine());
+					System.out.println("Introduce Nota:");
+					float nota = sc.nextInt();sc.nextLine();
+					//Comprobar si el alumnos tiene ya alguna nota
+					//en esa asignatura
+					//Si no tiene ninguna nota, hacemos insert
+					//Si tiene alugna nota hacemos update modificando
+					//el array (añadiendo un elemento al array)
+					Nota n = ad.obtenerNota(a, asig);
+					if(n==null) {
+						//Insert
+					}
+					else {
+						//Update
+					}
+				}
+				else {
+					System.out.println("Error, asignatura no existe");
+				}
+			}
+			else {
+				System.out.println("Error, alumno no existe");
+			}
+		} catch (ParseException e) {
+			System.out.println("Fecha Incorrectqa");
+		}
+		
 	}
 
 	private static void mostrarAlumnos() {
