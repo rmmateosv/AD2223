a250616342b6ff8535174da8a1604ac65285cd7e
rmmateosv@gmail.com <rmmateosv@gmail.com>
2023-03-01 13:08:16 +0100

    0103

--- workspace/Unidad5_DB4O/src/spotifly/Principal.java
+++ workspace/Unidad5_DB4O/src/spotifly/Principal.java
@@ -18,12 +18,17 @@ public class Principal {
 			do {
 				System.out.println("Introduce opción:");
 				System.out.println("0-Salir");
+				System.out.println("1-Crear Álbum");
+				System.out.println("2-Crear Canción");
 				
 				opcion = t.nextInt();
 				t.nextLine();
 				switch (opcion) {
 				case 1:
 					crearAlbum();
+					break;
+				case 2:
+					crearCancion();
 					break;
 				
 				}
@@ -32,6 +37,43 @@ public class Principal {
 			sf.cerrar();
 		} else {
 			System.out.println("Error, no hay conexión con SpotiFly");
+		}
+	}
+	private static void crearCancion() {
+		// TODO Auto-generated method stub
+		mostrarAlbumes();
+		System.out.println("Introduce artista:");
+		String artista = t.nextLine();
+		System.out.println("Introduce título:");
+		String titulo = t.nextLine();
+		Album al = sf.obtenerAlbum(artista, titulo);
+		if(al!=null) {
+			System.out.println("Introduce el título de la canción");
+			String titC = t.nextLine();
+			Cancion c = sf.obtenerCancion(al, new Cancion(titC,0));
+			if(c==null) {
+				//Añadimos la canción al álbum
+				al.getCanciones().add(new Cancion(titC, 0));
+				if(sf.modificarAlbum(al)) {
+					System.out.println("Canción añadida");
+				}
+				else {
+					System.out.println("Error al añadir la canción");
+				}
+			}
+			else {
+				System.out.println("Error, ya existe la canción");
+			}
+		}
+		else {
+			System.out.println("Error, álbum no existe");
+		}
+	}
+	private static void mostrarAlbumes() {
+		// TODO Auto-generated method stub
+		ArrayList<Album> albumes = sf.obtenerAlbumes();
+		for(Album al:albumes) {
+			al.mostrar(false);
 		}
 	}
 	private static void crearAlbum() {
