44a1a7e49c3dfdb57ef5e7bf303a5e2949aa5d91
rmmateosv@gmail.com <rmmateosv@gmail.com>
2023-05-04 13:20:01 +0200

    1204

--- workspace/RecuperacionMongoFacturas/src/Examen/Detalle.java
+++ workspace/RecuperacionMongoFacturas/src/Examen/Detalle.java
@@ -2,4 +2,38 @@ package Examen;
 
 public class Detalle {
 
+	private String producto;
+	private int cantidad;
+	private float precioUnidad;
+	public Detalle() {
+	}
+	public Detalle(String producto, int cantidad, float precioUnidad) {
+		this.producto = producto;
+		this.cantidad = cantidad;
+		this.precioUnidad = precioUnidad;
+	}
+	public String getProducto() {
+		return producto;
+	}
+	public void setProducto(String producto) {
+		this.producto = producto;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	public float getPrecioUnidad() {
+		return precioUnidad;
+	}
+	public void setPrecioUnidad(float precioUnidad) {
+		this.precioUnidad = precioUnidad;
+	}
+	@Override
+	public String toString() {
+		return "Detalle [producto=" + producto + ", cantidad=" + cantidad + ", precioUnidad=" + precioUnidad + "]";
+	}
+	
+	
 }
