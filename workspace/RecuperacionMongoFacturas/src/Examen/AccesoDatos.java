7d8a38ddcf7e4ee29a067d74bd9fc1fb53774b34
rmmateosv@gmail.com <rmmateosv@gmail.com>
2023-05-04 14:25:27 +0200

    1204

--- workspace/RecuperacionMongoFacturas/src/Examen/AccesoDatos.java
+++ workspace/RecuperacionMongoFacturas/src/Examen/AccesoDatos.java
@@ -1,5 +1,111 @@
 package Examen;
 
+import static com.mongodb.MongoClientSettings.getDefaultCodecRegistry;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+import org.bson.codecs.configuration.CodecProvider;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.bson.conversions.Bson;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.result.InsertOneResult;
+
 public class AccesoDatos {
 
+	private MongoClient cluster = null;
+	private MongoDatabase cnx = null;
+	private String nombreDB = "facturas";
+	// OBTENER EL CONNECTION STRING DEL LOCAL
+	private String cadenaCNX = "mongodb://localhost:27017";
+
+	public AccesoDatos() {
+
+		try {
+
+			cluster = MongoClients.create(new ConnectionString(cadenaCNX));
+
+			CodecProvider proveedor = PojoCodecProvider.builder().automatic(true).build();
+
+			CodecRegistry registro = fromRegistries(getDefaultCodecRegistry(), fromProviders(proveedor));
+
+			// Obtenemos la base de datos, si no existe se crea
+
+			cnx = cluster.getDatabase(nombreDB).withCodecRegistry(registro);
+
+		} catch (Exception e) {
+
+			System.out.println(e.getMessage());
+
+		}
+
+	}
+
+	public MongoDatabase getCnx() {
+
+		return cnx;
+
+	}
+
+	public void setCnx(MongoDatabase Cnx) {
+
+		this.cnx = cnx;
+
+	}
+
+	public void cerrar() {
+
+		try {
+
+			cluster.close();
+
+		} catch (MongoException e) {
+
+			System.out.println(e.getMessage());
+
+		}
+	}
+
+	public Productos obtenerProducto(String cod) {
+		// TODO Auto-generated method stub
+		Productos resultado = null; 
+		try {
+			
+			MongoCollection<Productos> col = cnx.getCollection("producto",Productos.class);
+			Bson filtro=Filters.eq("codigo",cod);
+			 resultado = col.find(filtro).first();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return resultado;
+	}
+
+	public boolean crearProducto(Productos pro) {
+		// TODO Auto-generated method stub
+		boolean resultado = false; 
+		try {
+			
+			MongoCollection<Productos> col = cnx.getCollection("producto",Productos.class);
+			InsertOneResult r= col.insertOne(pro);
+			if(r.getInsertedId()!=null) {
+				resultado=true;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return resultado;
+		
+		
+	}
 }
--- workspace/RecuperacionMongoFacturas/src/Examen/Principal.java
+++ workspace/RecuperacionMongoFacturas/src/Examen/Principal.java
@@ -1,5 +1,105 @@
 package Examen;
 
-public class Principal {
+import java.util.Date;
+import java.util.Scanner;
 
+public class Principal {
+static Scanner t = new Scanner(System.in);
+static AccesoDatos ad = new AccesoDatos();
+	 
+public static  void main(String[]args) {
+		int opcion;
+		
+		do {
+			System.out.println("Introduce opción:");
+			System.out.println("0-Salir");
+			System.out.println("1- Crear producto");
+			System.out.println("2- Crear factura");
+			System.out.println("3- Anular factura");
+			System.out.println("4- Informe de facturación");
+			opcion = t.nextInt();
+			t.nextLine();
+			switch (opcion) {
+			case 1:
+				CrearProducto();
+				break;
+			case 2:
+				CrearFactura();
+				break;
+			case 3:
+				AnularFactura();
+				break;
+			case 4:
+				InformeDeFacturacion();
+				break;
+
+			}
+		} while (opcion != 0);
+	}
+
+	private static void InformeDeFacturacion() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void AnularFactura() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void CrearFactura() {
+		// TODO Auto-generated method stub
+		Facturas fact= new Facturas();
+		
+		System.out.println("Indique el DNI del cliente");
+		fact.setCliente(t.nextLine());
+		fact.setFecha(new Date());
+		fact.setFacturaAnulacion(0);
+		fact.setNumero(ad.obtenerNumeroFactura());
+		String elec;
+		do {
+			
+			mostrarProductos();
+			
+			
+			System.out.println("Deseas introducir mas productos(0-no)");
+			elec=t.nextLine();
+		}while(!elec.equals("0"));
+	}
+
+	private static void mostrarProductos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void CrearProducto() {
+		// TODO Auto-generated method stub
+		System.out.println("Indique el codigo del producto");
+		String cod=t.nextLine();
+		//procedemos a comprobar si ya exisite el producto
+		Productos pro =ad.obtenerProducto(cod);
+		if(pro==null) {
+			pro= new Productos();
+			pro.setCodigo(cod);
+			System.out.println("Indique el nombre del producto");
+			pro.setNombre(t.nextLine());
+			System.out.println("Indique el precio");
+			pro.setPrecio(t.nextFloat());t.nextLine();
+			System.out.println("Indique el stock");
+			pro.setStock(t.nextInt());t.nextLine();
+			
+			if(ad.crearProducto(pro)) {
+				System.out.println("El producto indicado se ha creado exitosamente");
+				
+			}
+			else {
+				System.out.println("Error al crear el producto");
+			}
+		}
+		
+		else {
+			System.out.println("Error el producto indicado ya existe ");
+		}
+		
+	}
 }
