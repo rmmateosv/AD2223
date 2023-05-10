b09ffc0036642d5d687790db919cd644ba3bb910
rmmateosv@gmail.com <rmmateosv@gmail.com>
2023-05-10 11:06:40 +0200

    1204

--- workspace/ExamenHibernateTenis/src/examen/Partido.java
+++ workspace/ExamenHibernateTenis/src/examen/Partido.java
@@ -1,5 +1,70 @@
 package examen;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Partido {
 
+	private int codigo;
+	private Date fecha;
+	private int num_Set;
+	private Jugador ganador;
+	private List<JugadorPartido> jugadores= new ArrayList();
+	public Partido() {
+	}
+	public Partido(int codigo, Date fecha, int num_Set, Jugador ganador, List<JugadorPartido> jugadores) {
+		this.codigo = codigo;
+		this.fecha = fecha;
+		this.num_Set = num_Set;
+		this.ganador = ganador;
+		this.jugadores = jugadores;
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public int getNum_Set() {
+		return num_Set;
+	}
+	public void setNum_Set(int num_Set) {
+		this.num_Set = num_Set;
+	}
+	public Jugador getGanador() {
+		return ganador;
+	}
+	public void setGanador(Jugador ganador) {
+		this.ganador = ganador;
+	}
+	public List<JugadorPartido> getJugadores() {
+		return jugadores;
+	}
+	public void setJugadores(List<JugadorPartido> jugadores) {
+		this.jugadores = jugadores;
+	}
+	@Override
+	public String toString() {
+		
+		String resultado=  "Partido [codigo=" + codigo + ", fecha=" + fecha + ", num_Set=" + num_Set + ", ganador=" + ganador.getNombre()
+				+ "]";
+		resultado += "\n Jugadores:";
+		for(JugadorPartido jp :jugadores) {
+			resultado+= "\n"+ jp.getCjp().getJugador().getNombre();
+		}
+		
+		return resultado;
+	}
+	
+	
+	
+	
+	
 }
